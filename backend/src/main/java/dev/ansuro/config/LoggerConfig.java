8449f23ea28727b4721649bcb894644aaaa7972f
ansuro <Andy@codebox.sunshine.dev>
2017-02-04 18:58:50 +0100

    Logging aspect added

--- backend/src/main/java/dev/ansuro/config/LoggerConfig.java
+++ backend/src/main/java/dev/ansuro/config/LoggerConfig.java
@@ -1,18 +1,28 @@
 package dev.ansuro.config;
 
+import dev.ansuro.aop.LoggingAspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class LoggerConfig {
 
-	@Bean
-	@Scope("prototype")
-	public Logger log(InjectionPoint injectionPoint) {
-		return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
-	}
+    @Bean
+    @Scope("prototype")
+    public Logger log(InjectionPoint injectionPoint) {
+        return LoggerFactory.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+    }
+
+    @Bean
+    @Profile("dev")
+    public LoggingAspect loggingAspect() {
+        return new LoggingAspect();
+    }
 }
